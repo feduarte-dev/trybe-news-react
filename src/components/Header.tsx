36f4b650c38d45c32b2d5e4c745afb1f451380ac
Felipe Duarte <feduarte.dev@gmail.com>
2023-10-16 18:31:36 -0300

    fix trybe-logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,11 @@ import '../styles/header.css';
 function Header() {
   return (
     <div className="header-container">
-      <img src="src/assets/trybe.svg" alt="trybe-logo" className="trybe-logo" />
+      <img
+        src="src/public/static/images/trybe.svg"
+        alt="trybe-logo"
+        className="trybe-logo"
+      />
       <h1 className="site-title">TRYBE NEWS</h1>
     </div>
   );
